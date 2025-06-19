654bcac4a7474add49aeab7549b22ac267690417
dilshad-knk <dilshadali.knk@gmail.com>
2025-06-19 21:41:54 +0530

    Remove build

--- build/interfaces/ITask.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Priority;
-(function (Priority) {
-    Priority["High"] = "high";
-    Priority["Low"] = "low";
-})(Priority || (Priority = {}));
